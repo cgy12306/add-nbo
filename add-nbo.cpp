29d436c1018499f4b8ff30a8db6d647f44e30dc9
cgy12306 <cgy12306@naver.com>
2020-07-13 20:58:50 +0900

    Modify add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -3,25 +3,23 @@
 #include <fcntl.h>
 
 uint32_t change(uint32_t n){
+
 	return (n & 0xFF000000) >> 24 | (n & 0x00FF0000) >>8 | (n & 0x0000FF00) << 8 | (n & 0x000000FF) << 24;
 }
 
 int main(int argc, char *argv[]){
 	
 	FILE *fd1, *fd2;
-	uint32_t buf1[9], buf2[9];
-	uint32_t *p1 = reinterpret_cast<uint32_t*>(buf1);
-	uint32_t *p2 = reinterpret_cast<uint32_t*>(buf2);
-	uint32_t sum;
+	uint32_t buf1, buf2;
 
 	fd1 = fopen(argv[1], "r");
 	fd2 = fopen(argv[2], "r");
 
-	fread(buf1, 1, 8, fd1);
-	fread(buf2, 1, 8, fd2);
+	fread(&buf1, 1, 8, fd1);
+	fread(&buf2, 1, 8, fd2);
 
-	uint32_t n1 = change(*p1);
-	uint32_t n2 = change(*p2);
+	uint32_t n1 = change(buf1);
+	uint32_t n2 = change(buf2);
 
 	printf("%d(%p) + %d(%p) = %d(%p) \n", n1, n1, n2, n2, n1+n2, n1+n2);
 
